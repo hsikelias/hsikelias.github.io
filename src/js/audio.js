ccbd8919cb4c83141cdd92b90231d79a96e284fc
hsikelias <plekishsai@gmail.com>
2026-09-06 22:48:36 -0400

    added working audio interactios, clicks anywhere in the website works.. hoover has a bug: currently works only on the herot text; fix later

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,4 +1,18 @@
-function click_play(){
-    // const click = document.getElementById("click-sound");
-    // click-sound.play();
-}
+const audio = new Audio('src/assets/audio/hover-sharp.mp3');
+
+const hoverElements = document.querySelectorAll('.hover-sound');
+const hoverSound = new Audio('src/assets/audio/hover-wavy.wav');
+
+
+hoverElements.forEach(element => {
+    element.addEventListener('mouseenter', () => {
+        hoverSound.currentTime = 0;
+        hoverSound.play().catch(error => console.log("Hover audio failed"));
+    });
+});
+
+
+document.addEventListener('click', () => {
+    audio.play();
+    console.log("CLICK!"); 
+});
